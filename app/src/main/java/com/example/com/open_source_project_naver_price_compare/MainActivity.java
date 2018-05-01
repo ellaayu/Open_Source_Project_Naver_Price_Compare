57e56842d148666a5c601e9b8f4722da8490cfa9
com-PC\com <ella.ayuningtyas.eru@gmail.com>
2018-05-01 23:17:18 +0900

    Main Activity

---
+++ app/src/main/java/com/example/com/open_source_project_naver_price_compare/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.com.open_source_project_naver_price_compare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        databaseRef = database.getReference("massage");
+
+        databaseRef.setValue("Hello world");
+
+    }
+}
