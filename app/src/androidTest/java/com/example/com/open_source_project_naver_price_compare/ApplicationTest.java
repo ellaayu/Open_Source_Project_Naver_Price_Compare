1b5e2d755cc1e22386ea91261aec70c55257b8ab
com-PC\com <ella.ayuningtyas.eru@gmail.com>
2018-05-01 23:17:55 +0900

    no message

---
+++ app/src/androidTest/java/com/example/com/open_source_project_naver_price_compare/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.com.open_source_project_naver_price_compare;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
